15e456c1512c59dc6c2d4d3a71ab70484548c601
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-03 16:41:28 +0530

    auth page route gaurd

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import Image from "next/image";
 import AuthModal from "@/components/Modals/AuthModal";
 import { useRecoilValue } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
 
 type Props = {};
 
 const AuthPage = (props: Props) => {
   const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!pageLoading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
+  if (pageLoading) return null;
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
