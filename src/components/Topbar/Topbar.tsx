95ac90b79e61a081178c4ac88736ca9c9a6fd70a
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-05 13:54:04 +0530

    topbar userprofile functionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Link from "next/link";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
 type Props = {};
 
 const Topbar = (props: Props) => {
+  const [user] = useAuthState(auth);
   return (
     <nav className="relative flex h-[70px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
       <div
@@ -23,11 +26,28 @@ const Topbar = (props: Props) => {
               Premium
             </a>
           </div>
-          <Link href="/auth">
-            <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
-              Sign In
-            </button>
-          </Link>
+          {!user && (
+            <Link href="/auth">
+              <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
+                Sign In
+              </button>
+            </Link>
+          )}
+          {user && (
+            <div className="cursor-pointer group relative">
+              <img
+                src="/avatar.png"
+                alt="user profile"
+                className="h-8 w-8 rounded-full"
+              />
+              <div
+                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
+		transition-all duration-300 ease-in-out"
+              >
+                <p className="text-sm">{user.email}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </nav>
