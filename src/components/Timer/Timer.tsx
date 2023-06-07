dae7cbf3e79743a871cc9c00bbd9c8d19fc10534
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-07 11:51:49 +0530

    timer functionality

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,19 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiRefreshCcw } from "react-icons/fi";
 
 type Props = {};
 
 const Timer = (props: Props) => {
-  const [showTimer, setShowTimer] = useState<boolean>(true);
+  const [showTimer, setShowTimer] = useState<boolean>(false);
+  const [time, setTime] = useState<number>(0);
+
+  const formatTime = (time: number): string => {
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    return `${hours < 10 ? "0" + hours : hours}:${
+      minutes < 10 ? "0" + minutes : minutes
+    }:${seconds < 10 ? "0" + seconds : seconds}`;
+  };
+
+  const handleClockClick = () => {
+    setShowTimer(true);
+  };
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (showTimer) {
+      intervalId = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [showTimer]);
   return (
     <div>
       {showTimer ? (
-        <div className="flex items-center space-x-2 bg-dark-fill-3 py-1.5 cursor-pointer rounded hover:bg-dark-fill-2">
-          <div>00:10:12</div>
-          <FiRefreshCcw />
+        <div className="flex items-center space-x-2 bg-dark-fill-3 p-1.5 cursor-pointer rounded hover:bg-dark-fill-2">
+          <div>{formatTime(time)}</div>
+          <FiRefreshCcw
+            onClick={() => {
+              setShowTimer(false);
+              setTime(0);
+            }}
+          />
         </div>
       ) : (
-        <div className="flex items-center p-1 h-8 hover:bg-dark-fill-3 rounded cursor-pointer">
+        <div
+          className="flex items-center p-1 h-8 hover:bg-dark-fill-3 rounded cursor-pointer"
+          onClick={handleClockClick}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
