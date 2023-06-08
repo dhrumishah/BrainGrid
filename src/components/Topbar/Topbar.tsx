f64140045cc173fa1beef18148f36b2f3d970a5c
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-08 10:11:45 +0530

    Test cases UI

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -50,7 +50,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
         <div className="flex items-center space-x-4 flex-1 justify-end">
           <div>
             <a
-              href="https://www.buymeacoffee.com/burakorkmezz"
+              href="#"
               target="_blank"
               rel="noreferrer"
               className="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2"
--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -4,13 +4,15 @@ import Split from "react-split";
 import CodeMirror from "@uiw/react-codemirror";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
 import { javascript } from "@codemirror/lang-javascript";
+import EditorFooter from "./EditorFooter";
 
 type Props = {};
 
 const Playground = (props: Props) => {
   return (
-    <div className="flex flex-col bg-dark-layer-1 relative">
+    <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
       <PreferenceNav />
+
       <Split
         className="h-[calc(100vh-94px)]"
         direction="vertical"
@@ -19,14 +21,60 @@ const Playground = (props: Props) => {
       >
         <div className="w-full overflow-auto">
           <CodeMirror
-            value="const a = 1;"
+            value="const a = 1 ;"
             theme={vscodeDark}
             extensions={[javascript()]}
             style={{ fontSize: 16 }}
           />
         </div>
-        <div>testcases</div>
+        <div className="w-full px-5 overflow-auto">
+          <div className="flex h-10 items-center space-x-6">
+            <div className="relative flex h-full flex-col justify-center cursor-pointer">
+              <div className="text-sm font-medium leading-5 text-white">
+                Testcases
+              </div>
+              <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="mr-2 items-start mt-2 text-white">
+              <div className="flex items-center flex-wrap gap-y-4">
+                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  Case 1
+                </div>
+              </div>
+            </div>
+
+            <div className="mr-2 items-start mt-2 text-white">
+              <div className="flex items-center flex-wrap gap-y-4">
+                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  Case 2
+                </div>
+              </div>
+            </div>
+
+            <div className="mr-2 items-start mt-2 text-white">
+              <div className="flex items-center flex-wrap gap-y-4">
+                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  Case 3
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="font-semibold my-4">
+            <p className="text-sm font-medium mt-4 text-white">Input:</p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              nums: [2, 7, 11, 15], target: 9
+            </div>
+            <p className="text-sm font-medium mt-4 text-white">Output:</p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              [0, 1]
+            </div>
+          </div>
+        </div>
       </Split>
+      <EditorFooter />
     </div>
   );
 };
