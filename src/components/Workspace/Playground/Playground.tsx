93bb89540cb76a6f17a5a017c47abcac6f6a7d67
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-12 14:03:34 +0530

    changing UI of testcases

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PreferenceNav from "./PreferenceNav/PreferenceNav";
 import Split from "react-split";
 import CodeMirror from "@uiw/react-codemirror";
@@ -12,6 +12,7 @@ type PlaygroundProps = {
 };
 
 const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
+  const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
   return (
     <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
       <PreferenceNav />
@@ -42,11 +43,16 @@ const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
           <div className="flex">
             {problem.examples.map((example, index) => (
               <div
-                className="mr-2 items-start mt-2 text-white"
+                className="mr-2 items-start mt-2 text-gray-500"
                 key={example.id}
+                onClick={() => setActiveTestCaseId(index)}
               >
                 <div className="flex items-center flex-wrap gap-y-4">
-                  <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  <div
+                    className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
+                    ${activeTestCaseId === index ? "text-white" : ""}
+                    `}
+                  >
                     Case {index + 1}
                   </div>
                 </div>
@@ -57,11 +63,11 @@ const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
           <div className="font-semibold my-4">
             <p className="text-sm font-medium mt-4 text-white">Input:</p>
             <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
-              nums: [2, 7, 11, 15], target: 9
+              {problem.examples[activeTestCaseId].inputText}
             </div>
             <p className="text-sm font-medium mt-4 text-white">Output:</p>
             <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
-              [0, 1]
+              {problem.examples[activeTestCaseId].outputText}
             </div>
           </div>
         </div>
