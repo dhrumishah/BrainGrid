7fa4b51e824b16331076ed110e2c5e973dfbd2a9
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-06-14 20:15:37 +0530

    added confetti on success

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Split from "react-split";
 import ProblemDescription from "./Problemdescription/ProblemDescription";
 import Playground from "./Playground/Playground";
 import { Problem } from "@/utils/types/problem";
+import Confetti from "react-confetti";
 type WorkSpaceProps = {
   problem: Problem;
 };
 
 const Workspace: React.FC<WorkSpaceProps> = ({ problem }) => {
+  const [success, setSuccess] = useState(false);
   return (
     <Split className="split" minSize={0}>
       <ProblemDescription problem={problem} />
       <Playground problem={problem} />
+      {success && <Confetti gravity={0.3} tweenDuration={4000} />}
     </Split>
   );
 };
